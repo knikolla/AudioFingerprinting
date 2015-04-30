30b87ac9d4d5851fcc8ff84ad27cc3f51f2b3a3e
Kristi Nikolla <knikolla@bu.edu>
2015-04-30 14:46:06 -0400

    minor improvements

--- aufin/find.py
+++ aufin/find.py
@@ -3,6 +3,7 @@ import pyaudio
 
 from analyze import read_wav, get_spectrogram, get_peaks
 
+
 def find(signal):
     x = get_spectrogram(signal)
     y = get_peaks(x)
@@ -42,6 +43,8 @@ def find(signal):
         result = db.get_song(best_song)
         print("Title: " + result["title"] + "\tAuthor: " + result["author"] + "\tAlbum: " + result["album"] +
               "\t\t\t(" + str(best_count) + " fingerprint hits)")
+    else:
+        print("No confident match found...")
 
 
 def from_file(file, start=0, end=0):
